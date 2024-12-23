812be4faa56dc8c65c61aaf3ddbe551ca845e694
Abhay Singh <abhaysingh42852@gmail.com>
2024-12-23 18:55:32 +0530

    Initial Commit SET-UP

---
+++ frontEnd/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='felx items-center justify-between py-5 font-medium'>
+        navbaar
+      
+    </div>
+  )
+}
+
+export default Navbar

77b80eb0eb108fe3be3e16459146820030838cc0
Abhay Singh <abhaysingh42852@gmail.com>
2024-12-30 21:42:35 +0530

    Product Page Added

--- frontEnd/src/pages/Product.jsx
+++ frontEnd/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
 
@@ -84,6 +85,7 @@ const Product = () => {
         </div>
       </div>
       {/* Display Related Products */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
     </div>
   ):<div className='opacity-0'></div>
 }

9190e91d022fe01657ffb2e485a7d7018bbb1c5e
Abhay Singh <abhaysingh42852@gmail.com>
2025-01-11 14:40:26 +0530

    Place Order UI Added

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -5,16 +5,18 @@ import Collection from './Pages/Collection'
 import Product from './Pages/Product'
 import Login from './Pages/Login'
 import Contact from './Pages/Contact'
-import Cart from './Pages/Cart'
-import PlaceOrder from './Pages/PlaceOrder'
+import Cart from './pages/Cart'
+import PlaceOrder from './pages/PlaceOrder'
 import Orders from './Pages/Orders'
 import About from './Pages/About'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Searchbar from './components/Searchbar'
+import { ToastContainer, toast } from 'react-toastify';
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer/>
       <Navbar/>
       <Searchbar/>
       <Routes>
@@ -26,7 +28,7 @@ const App = () => {
       <Route path='/cart' element={<Cart/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/place-order' element={<PlaceOrder/>}/>
-      <Route path='orders' element={<Orders/>}/>
+      <Route path='/orders' element={<Orders/>}/>
       </Routes>
       <Footer/>
     </div>
--- frontEnd/src/components/Navbar.jsx
+++ frontEnd/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink} from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-  const {setShowSearch,showSearch}=useContext(ShopContext)
-  const navigate=useNavigate();
+  const {setShowSearch,navigate,getCartCount}=useContext(ShopContext)
+  
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to='/'><img src={assets.logo} className="w-36" alt="logo" /></Link>
@@ -53,7 +53,7 @@ const Navbar = () => {
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} alt="cart-icon" className="w-5 min-w-5" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[9px]">
-            5
+            {getCartCount()}
           </p>
         </Link>
         <img

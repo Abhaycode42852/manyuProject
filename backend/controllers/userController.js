13cb725da73132352952c61e6311783fdb43bab2
Abhay Singh <abhaysingh42852@gmail.com>
2025-02-19 18:18:26 +0530

    backed Admin Panel Completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -80,6 +80,19 @@ const userSignUp=async (req,res)=>{
 // Admin Login
 
 const adminLogin=async(req,res)=>{
+    try {
+        const {email , password}=req.body
+        if (email===process.env.ADMIN_EMAIL && password===process.env.ADMIN_PASSWORD) {
+            const token=jwt.sign(email+password,process.env.JWT_SECRET)
+            res.json({success:true,token})
+        }
+        else{
+            res.json({success:false,message:"Invalid Credentials"})
+        }
+    } catch (error) {
+        console.error(error)
+        res.json({sucess:false,message:error.message})
+    }
 
 }
 
---
+++ backend/middleware/adminAuth.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken"
+
+const adminAuth=(req,res,next)=>{
+try {
+    const token=req.headers
+    if(!token){
+        return res.json({success:false,message:"Invalid Credential"})
+    }
+    const trueToken=jwt.verify(token,process.env.JWT_SECRET)
+    if (trueToken!==process.env.ADMIN_EMAIL+process.env.ADMIN_PASSWORD) {
+        return res.json({success:false,message:"Invalid Credential"})
+    }
+    next()
+} catch (error) {
+    console.error(error)
+        res.json({sucess:false,message:error.message})
+}
+}
+
+
+export default adminAuth

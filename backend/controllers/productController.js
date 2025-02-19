ace73d617a6bd4ec6e7b2ef1afd29c8113b55532
Abhay Singh <abhaysingh42852@gmail.com>
2025-02-20 04:42:01 +0530

    Errors were resolved

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,11 +5,10 @@ const addProduct= async(req,res)=>{
     try {
         
         const {name, description,price,category,subcategory,sizes,bestseller}=req.body
-        
-        const image1=req.files.image1&&req.files.image1[0]
-        const image2=req.files.image2&req.files.image2[0]
-        const image3=req.files.image3&&req.files.image3[0]
-        const image4=req.files.image4&&req.files.image4[0]
+        const image1=req.files.image1 && req.files.image1[0]
+        const image2=req.files.image2 && req.files.image2[0]
+        const image3=req.files.image3 && req.files.image3[0]
+        const image4=req.files.image4 && req.files.image4[0]
 
         const images=[image1,image2,image3,image4].filter((item)=>item!==undefined)
         
@@ -27,7 +26,7 @@ const addProduct= async(req,res)=>{
         const product=new productModel(productData);
         await product.save()
 
-        res.json({sucess:true,message:"Product Added Sucessfully"})
+        res.json({success:true,message:"Product Added Sucessfully"})
 
     } catch (error) {
         console.error(error)
